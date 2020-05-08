43a5664d4a1ebaa6036181614edb4964f0bfc957
windows7lake <windows7lake@gmail.com>
2020-05-08 17:35:02 +0800

    修改dart使用方式

--- lib/flutterperferences.dart
+++ lib/flutterperferences.dart
@@ -3,44 +3,28 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Flutterperferences {
-  Flutterperferences._();
+  static const MethodChannel _channel = const MethodChannel(
+      'flutterperferences');
+  static Map<String, dynamic> _cache;
 
-  static Flutterperferences _instance;
-
-  factory Flutterperferences() {
-    if (_instance == null) {
-      _instance = Flutterperferences._();
-//      _instance._getCache;
-    }
-    return _instance;
-  }
-
-  MethodChannel _channel = const MethodChannel('flutterperferences');
-  Map<String, dynamic> _cache;
-
-  Future<void> get getCache async {
+  static Future<void> get getCache async {
     var cache = await _channel.invokeMethod('getCache');
-    if (cache != null) {
-//      _cache = cache;
-      _cache = new Map<String, dynamic>.from(cache);
-    } else {
-      _cache = {};
-    }
+    _cache = cache != null ? Map<String, dynamic>.from(cache) : {};
   }
 
-  bool get isNewUser {
+  static bool get isNewUser {
     return _cache["isNewUser"] ?? false;
   }
 
-  bool get isSelectLabel {
+  static bool get isSelectLabel {
     return _cache["isSelectLabel"] ?? false;
   }
 
-  String get refreshToken {
+  static String get refreshToken {
     return _cache["refreshToken"] ?? "";
   }
 
-  String get accessToken {
+  static String get accessToken {
     return _cache["accessToken"] ?? "";
   }
 }
